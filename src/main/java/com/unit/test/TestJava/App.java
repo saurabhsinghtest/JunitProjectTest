0c44336d271c7e056ccaa1530977b9fa8983bac3
Parina Mangla <parina.mangla@macys.com>
2017-12-12 14:46:19 +0530

    First commit

---
+++ src/main/java/com/unit/test/TestJava/App.java
@@ -0,0 +1,13 @@
+package com.unit.test.TestJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/unit/test/TestJava/HelloJunit.java
@@ -0,0 +1,14 @@
+package com.unit.test.TestJava;
+
+import org.junit.Test;
+
+
+public class HelloJunit  {
+
+	@Test
+	public void test() {
+		System.out.println("Not yet implemented");
+		String testEnv = System.getProperty("mySystemProperty");
+		System.out.println(testEnv);
+	}
+}
